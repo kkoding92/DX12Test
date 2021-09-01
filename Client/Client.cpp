957ede431a8ff3df324dada06a5b417cbd919b52
kt0811 <hansam2396@gmail.com>
2021-09-02 01:55:30 +0900

    Fix Message Loop

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -43,13 +43,21 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     MSG msg;
 
     // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    while (true)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+            if (msg.message == WM_QUIT)
+                break;
+
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
         }
+
+       //게임로직 실행
     }
 
     return (int) msg.wParam;
